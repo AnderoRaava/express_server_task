7781ce9b29c7252c371ed6c71efeb7e6176103e5
AnderoRaava <aegviidu@gmail.com>
2022-09-06 19:15:21 +0300

    Works now

--- src/api.js
+++ src/api.js
@@ -5,11 +5,7 @@ const app = express();
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({
-        // siin tuleb panna mingi muu response kui json
-            "hello":"amen"
-    }
-    )
+    res.send("Tere, " + req.query.information)
 });
 
 app.use("/.netlify/functions/api",router)
